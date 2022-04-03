99f1ea212a215d99c266116397a7a1e39b879b76
Roger Garcia <101257233+r0gergarc1a@users.noreply.github.com>
2022-04-03 18:07:48 +0200

    Joc ACABAT

---
+++ Escenes/No_vides.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func _process(delta):
+	if Input.is_action_pressed("enter"):
+		get_tree().change_scene('res://Escenes/Principal/Pantalla Principal.tscn')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
